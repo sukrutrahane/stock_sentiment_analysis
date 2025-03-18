b7fdbb8680aa31a78324d5c83a4a231a79a8bb4f
Sukrut Rahane <sukrutrahane@gmail.com>
2025-03-18 22:29:25 +0530

    configured port for deployment

--- backend_old/app/services/news_fetcher.py
+++ backend_old/app/services/news_fetcher.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
--- backend_old/run.py
+++ backend_old/run.py
@@ -1,7 +1,16 @@
+# from app import create_app
+
+# app = create_app()
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import os
 from app import create_app
 
 app = create_app()
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from Render
+    app.run(host="0.0.0.0", port=port, debug=True)  # Ensure it's accessible
